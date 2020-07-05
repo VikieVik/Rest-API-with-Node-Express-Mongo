972ca1df5498bb14b9345bc4976c461de62e3384
Singh-Vikas-M <vikas3091999@gmail.com>
2020-07-06 03:27:46 +0530

    added env sample and mongo url

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 //Middleware
 
 //Routes
 app.get("/", (req, res) => {
   res.send("we are live");
+});
+
+// connect to db
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  return console.log("connected to DB");
 });
 
 //start listening
